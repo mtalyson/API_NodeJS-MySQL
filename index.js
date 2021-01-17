971f054dccfb8338329da5b16ad000e552eb6350
Talyson Moreira <talyson.penha@gmail.com>
2021-01-17 19:33:50 -0300

    Project created

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, function(){
+   console.log("Servidor rodando na URL: http://localhost:3000")
+})
+
+app.get('/', (req, res) => {
+   res.send("Hello World!")
+})
