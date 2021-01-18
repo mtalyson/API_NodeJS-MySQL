92d2ea859eea5a92432d414e5f9e23bb05ea8a51
Talyson Moreira <talyson.penha@gmail.com>
2021-01-17 22:41:36 -0300

    Implemented /GET Task and /GET Task Id

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,17 +1,38 @@
 const database = require('../database/connection')
 
 class TaskController {
-   newTask(req, res) {
-      
+   newTask(req, res) {   
       const {task, description, responsible} = req.body
-      
-      database.insert({task, description, responsible}).table("tasks")
-         .then(()=>{
-            res.json({message: "Task successfully created!"})
+
+      database
+      .insert({task, description, responsible})
+      .table("tasks")
+      .then(()=>{
+         res.json({message: "Task successfully created!"})
          }).catch(error => {
             console.log(error)
          })
+   }
 
+   listTasks(req, res){
+      database.select("*").table("tasks")
+      .then(tasks=>{
+         res.json(tasks)
+         }).catch(error=>{
+            console.log(error)
+         })
+   }
+
+   listTaskById(req, res){
+      const id = req.params.id
+
+      database.select("*").table("tasks")
+      .where({id:id})
+      .then(tasks=>{
+            res.json(tasks)
+         }).catch(error=>{
+            console.log(error)
+         })
    }
 }
 
--- src/routes/task.js
+++ src/routes/task.js
@@ -4,4 +4,8 @@ const TaskController = require('../controllers/taskController')
 
 router.post('/newTask', TaskController.newTask)
 
+router.get('/tasks', TaskController.listTasks)
+
+router.get('/task/:id', TaskController.listTaskById)
+
 module.exports = router
